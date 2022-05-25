a741e29807a331b4f5070100f65ed460579cc781
JiiBOTz <93678016+Mujipro78@users.noreply.github.com>
2022-05-26 00:22:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ global.sgh = process.env.LINK_GITHUB
 global.sgc = process.env.LINK_GROUP
 global.ownernumber = process.env.NOWNER
 global.owner = [
-  ['0'],
-  ['0'],
-  [`${process.env.NOWNER}`, `${process.env.NAMAOWNER}`, true]
+  ['6285763816560'],
+  ['6285763816560'],
+  [`${process.env.6285763816560}`, `${process.env.⏤͟͟͞͞Jiixxy͜͡🎭}`, true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [`${process.env.MODERATOR}`] // Want some help?
